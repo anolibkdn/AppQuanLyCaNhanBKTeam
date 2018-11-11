e7160c596fd9fde2557931c2d81a16ea6c01d7af
mrrobot <anolibkdn>
2018-11-11 15:53:27 +0700

    first commit

---
+++ appandroidfinal/app/src/main/java/com/bkteam/mrrobot/thongkechitieu/TimelinePage.java
@@ -0,0 +1,21 @@
+package com.bkteam.mrrobot.thongkechitieu;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TimelinePage extends Fragment {
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        view = inflater.inflate(R.layout.time_line_page,container,false);
+        return view;
+    }
+}
---
+++ hoanthienapp/app/src/main/java/com/bkteam/mrrobot/econom/MoneyClass.java
@@ -0,0 +1,4 @@
+package com.bkteam.mrrobot.econom;
+
+public class MoneyClass {
+}
